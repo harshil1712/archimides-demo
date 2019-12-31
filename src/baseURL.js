48ffc0ea03ab32047d1467d4dc189cb915f6fe6a
harshil1712 <ghagrawal17@gmail.com>
2020-01-01 02:51:51 +0530

    added input fields in login page

---
+++ src/baseURL.js
@@ -0,0 +1,3 @@
+const baseURL = 'https://test-archimides.free.beeceptor.com/api/'
+
+export default baseURL
--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,64 @@
-import React from 'react'
-export default () => (
-    <h1>LOgin</h1>
-)
+import React, { Component } from 'react'
+import axios from 'axios'
+import baseURL from '../baseURL'
+import { Redirect } from 'react-router-dom'
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          admin: false,
+          role: null
+        };
+    }
+
+    redirect(role){
+        if(role==='Admin'){
+            return <Redirect to="/admin/getStories" />
+        }
+        return <Redirect to="/app/2/getStories" />
+    }
+
+    loginUser(e){
+        e.preventDefault();
+        if(this.state.admin){
+            axios.post(baseURL+'admin-login', {
+                email:'a@a.a',
+                password:'a'
+            })
+            .then((res)=>{
+                var a = JSON.stringify(res.data)
+                console.log(typeof(JSON.parse(a)))
+                this.setState({role:'Admin'})
+                this.redirect(this.state.role)
+            })
+            .catch(err=>console.log(err))
+        }
+        else{
+            axios.post(baseURL+'login',
+            {
+                email:'a@a.com',
+                password:'abcd'
+            })
+            .then((res)=>{
+                console.log(res)
+                this.redirect(this.state.role)
+            })
+            .catch(err=>console.log(err))
+        }
+    }
+    render() {
+        return(
+            <div>
+                <h1>Login</h1>
+                <form onSubmit={this.loginUser.bind(this)}>
+                <input type="email" required/>
+                <input type="password" required/>
+                <input type="submit" value="submit" />
+                <input type="checkbox" onInput={()=>{this.setState({admin:true})}}/>Admin
+                </form>
+            </div>
+        )
+    }
+}
+export default Login
