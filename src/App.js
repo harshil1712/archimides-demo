de3267ac00be23b47b49137203ac6f65c507fe9f
harshil1712 <ghagrawal17@gmail.com>
2019-12-31 23:52:06 +0530

    basic routes configured

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/home'
+import Login from './components/login'
+import createStory from './components/createStory'
+import getStories from './components/getStories'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/app/:userId/createStory" component={createStory}/>
+        <Route path="/app/:userId/getStories" component={getStories}/>
+        <Route path="/admin/getStories" component={getStories}/>
+        <Route path="*" component={()=>(<h1>404 Page Not Found</h1>)} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/login.js
@@ -0,0 +1,4 @@
+import React from 'react'
+export default () => (
+    <h1>LOgin</h1>
+)
